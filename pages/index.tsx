0c4831b60a30372a476d99bf1808f0042b2095a6
shundroid <shundroid@users.noreply.github.com>
2022-08-06 13:16:54 +0900

    third commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { useState } from 'react'
 // import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [count, setCount] = useState(0)
   return (
     <div className={styles.container}>
       test hogehoge
+      {count}
+      <button onClick={() => { setCount(count + 1) }}>incr</button>
+      <Link href="/description">go to description</Link>
     </div>
   )
 }
